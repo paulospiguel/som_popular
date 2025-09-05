fbdf86d000c6fa70f94e17726f7a2bc5eb2f7317
PAULO SPIGUEL <41386612+paulospiguel@users.noreply.github.com>
2025-09-05 16:42:34 +0000

    fix gerbaseUrl

--- src/lib/base-url.ts
+++ src/lib/base-url.ts
@@ -1,8 +1,15 @@
 export function getBaseUrl() {
-  if (process.env.APP_URL) return process.env.APP_URL;
-  if (process.env.PUBLIC_NEXT_APP_URL) return process.env.PUBLIC_NEXT_APP_URL;
+  if (process.env.APP_URL) return `https://${process.env.APP_URL}`;
+  if (process.env.PUBLIC_NEXT_APP_URL) return `https://${process.env.PUBLIC_NEXT_APP_URL}`;
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-  if (process.env.NODE_ENV === "development") return "http://localhost:3000";
 
   return "http://localhost:3000";
+}
+
+export function getPublicUrl() {
+  return getBaseUrl().replace("http://", "https://");
+}
+
+export function getBaseUri() {
+  return getBaseUrl().replace(/^https?:\/\//, "");
 }
